b063567ac71f16653deab6b7f34c79439f90d765
Shawnsan <santiagoshawn@gmail.com>
2024-05-27 12:58:23 -0400

    update

--- extensions/moast-shopping-widget-block/frontend/entrypoints/main.js
+++ extensions/moast-shopping-widget-block/frontend/entrypoints/main.js
@@ -1,19 +1,56 @@
-import 'vite/modulepreload-polyfill'
-import './main.scss'
+import 'vite/modulepreload-polyfill';
+import profiles from '../data/profile.json';
+import { register } from 'swiper/element/bundle'// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+register();
 
-console.log('hello world');
-if (customElements.get('swiper-slide') !== undefined) {
-    console.log('SwiperSlide is already defined');
-    const SwiperSlide = customElements.get('swiper-slide');
-    console.log(SwiperSlide);
-    class MoastSlide extends SwiperSlide {
+if (customElements.get('moast-slider') == undefined) {
+    class MoastSlider extends HTMLElement {
         constructor() {
             super();
-            this.addEventListener('click', (event) => {
-                console.log(event);
-            });
+
+        }
+        connectedCallback() {
+            const slides = profiles.profiles.map((profile) => {
+                const slide = document.createElement('swiper-slide');
+                slide.classList.add('swiper-slide');
+                const img = document.createElement('img');
+                img.src = profile.member.avatars[0].url;
+                img.style.maxWidth = '100%';
+                img.style.maxHeight = '100%';
+                slide.appendChild(img);
+
+                return slide
+            })
+            const swiperContainer = document.createElement('swiper-container');
+
+            swiperContainer.innerHTML = ` ${slides.map((slide) => slide.outerHTML).join('')}`
+
+            this.shadowRoot.appendChild(swiperContainer);
+        }
+
+    }
+
+    customElements.define('moast-slider', MoastSlider);
+}
+if (customElements.get('moast-profile') === undefined) {
+    class MoastProfile extends HTMLElement {
+        constructor() {
+            super();
+
+        }
+        render() {
+            return `
+            <div>
+            
+            </div>
+            `
+        }
+        connectedCallback() {
+            this.innerHTML = this.render();
         }
     }
-    console.log('MoastSlide', MoastSlide);
-    customElements.define('moast-slide', MoastSlide);
+    customElements.define('moast-profile', MoastProfile);
 }
